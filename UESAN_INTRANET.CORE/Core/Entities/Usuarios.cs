b684c610e41e1d92c3e5438a683651100b957e21
J03_M4NT <jmantillah1503@gmail.com>
2025-07-02 01:10:29 -0500

    fix: conexion para registrar usuario completada sin problema

--- UESAN_INTRANET.CORE/Core/Entities/Usuarios.cs
+++ UESAN_INTRANET.CORE/Core/Entities/Usuarios.cs
@@ -1,33 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace UESAN_INTRANET.CORE.Core.Entities;
 
 public partial class Usuarios
 {
     public int UsuarioId { get; set; }
-
     public string? Nombre { get; set; }
-
     public string? Apellido { get; set; }
-
     public string Correo { get; set; } = null!;
-
     public string Contraseña { get; set; } = null!;
-
     public int RolId { get; set; }
-
     public bool? Estado { get; set; }
-
     public DateTime? FechaRegistro { get; set; }
 
     public virtual ICollection<Accesos> Accesos { get; set; } = new List<Accesos>();
-
     public virtual ICollection<Asignaciones> Asignaciones { get; set; } = new List<Asignaciones>();
-
     public virtual ICollection<ListasCerradasGuardadas> ListasCerradasGuardadas { get; set; } = new List<ListasCerradasGuardadas>();
-
     public virtual ICollection<Notificaciones> Notificaciones { get; set; } = new List<Notificaciones>();
 
-    public virtual Rol Rol { get; set; } = null!;
+    [JsonIgnore] // <-- Esto evita que sea requerido en el binding del controlador
+    public virtual Rol? Rol { get; set; }
 }
